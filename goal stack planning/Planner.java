7007f867325bbb87b5039d6166157b154b3b0fa0
SAURABH\Saurabh <saurabhkoshatwar1996@gmail.com>
2018-08-29 22:09:22 +0530

    gsp almost done

--- goal stack planning/Planner.java
+++ goal stack planning/Planner.java
@@ -7,9 +7,9 @@ int k;
 int[][] on;
 int[] ontable;
 int[] clear;
-int[] holding;
+char holding;
 int armempty;
-
+char[] alphabet;
 
 String start;
 String goal;
@@ -18,16 +18,16 @@ String goal;
 Scanner sc = new Scanner(System.in);
 	
 	Stack<String> goalstack = new Stack<>();
-
+	ArrayList<String> actions = new ArrayList<>();
 	Planner(int n)
 	{
 		k=n;
 		on = new int[n][n];
 		ontable = new int[n];
 		clear = new int[n];
-		holding = new int[n];
 		armempty = 1;	
-	
+		alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
 	}
 	public void input()
 	{
@@ -37,8 +37,8 @@ Scanner sc = new Scanner(System.in);
 		current = start;
 		System.out.println("Enter Start State");
 		goal = sc.next();*/
-		start = "ontable(a)^ontable(c)^on(b,a)^clear(a)^clear(C)^armempty";
-		goal = "clear(A)^ontable(C)^on(A,B)^on(B,C)^armempty";		
+		start = "ontable(a)^ontable(c)^on(a,b)^clear(a)^clear(c)^armempty";
+		goal = "clear(a)^ontable(c)^on(a,b)^on(b,c)^armempty";		
 	};
 		
 	public void set_initial()
@@ -48,19 +48,19 @@ Scanner sc = new Scanner(System.in);
 		{	
 			if(sg[i].contains("ontable"))
 			{
-				ontable[sg[i].charAt(8)] = 1;
+				ontable[sg[i].charAt(8)%97] = 1;
 			}
 			else if(sg[i].contains("on"))
 			{
-				on[sg[i].charAt(3)][sg[i].charAt(5)] = 1;
+				on[sg[i].charAt(3)%97][sg[i].charAt(5)%97] = 1;
 			}
 			else if(sg[i].contains("clear"))
 			{
-				clear[sg[i].charAt(6)] = 1;
+				clear[sg[i].charAt(6)%97] = 1;
 			}
 			else if(sg[i].contains("holding"))
 			{
-				holding[sg[i].charAt(8)] = 1;
+				holding = sg[i].charAt(8);
 			}
 			else if(sg[i].contains("armempty"))
 			{
@@ -74,11 +74,14 @@ Scanner sc = new Scanner(System.in);
 	{	
 		if(state.contains("^"))
 		{
-				
-			String simple_state[] = start.split("['^']");
+			System.out.println();
+			System.out.println(state);	
+			String simple_state[] = state.split("['^']");
 			
 			for(int i=0;i<simple_state.length;i++)
 			{
+				
+				System.out.println(simple_state[i]);
 				if(!check_simple(simple_state[i]))
 				{
 					return false;
@@ -96,19 +99,19 @@ Scanner sc = new Scanner(System.in);
 	
 	public boolean check_simple(String state)
 	{
-			if(state.contains("ontable") && ontable[state.charAt(8)] == 1)
+			if(state.contains("ontable") && ontable[state.charAt(8)%97] == 1)
 			{
 				return true;
 			}
-			else if(state.contains("on") && on[state.charAt(3)][state.charAt(5)] == 1)
+			else if(state.contains("on(") && on[state.charAt(3)%97][state.charAt(5)%97] == 1)
 			{
 				return true;
 			}
-			else if(state.contains("clear") && clear[state.charAt(6)] == 1)
+			else if(state.contains("clear") && clear[state.charAt(6)%97] == 1)
 			{
 				return true;
 			}
-			else if(state.contains("holding") && holding[state.charAt(8)] == 1)
+			else if(state.contains("holding") && holding==state.charAt(8)%97)
 			{
 				return true;
 			}
@@ -122,33 +125,161 @@ Scanner sc = new Scanner(System.in);
 			}			
 	}
 	
+	public void preAction(String action)
+	{
+		
+		if(action.contains("ontable"))
+			{
+				goalstack.push("putdown("+action.charAt(8)+")");
+				
+				//preconditions
+				goalstack.push("holding("+action.charAt(8)+")");
+			}
+			else if(action.contains("on"))
+			{
+				goalstack.push("stack("+action.charAt(3)+","+action.charAt(5)+")");
+				
+				//preconditions
+				goalstack.push("clear("+action.charAt(5)+")");
+				goalstack.push("holding("+action.charAt(3)+")");
+
+			}
+			else if(action.contains("clear"))
+			{
+				int i;
+				for( i = 0;i<k;i++)
+				{
+					if(on[i][action.charAt(6)%97]==1)
+					{
+						break;
+					}
+				}
+
+				goalstack.push("unstack("+alphabet[i]+","+action.charAt(6)+")");
+				
+				//preconditions
+				goalstack.push("on("+alphabet[i]+","+action.charAt(6)+")");
+				goalstack.push("clear("+action.charAt(i)+")");
+				goalstack.push("armempty");
+
+				
+			}
+			else if(action.contains("holding"))
+			{
+				goalstack.push("pickup("+action.charAt(8)+")");
+				
+				//preconditions
+				goalstack.push("ontable("+action.charAt(8)+")");
+				goalstack.push("armempty");
+
+			}
+			else if(action.contains("armempty"))
+			{
+				goalstack.push("putdown("+holding+")");
+				
+				//preconditions
+				goalstack.push("holding("+holding+")");
+			}
+
+	}
+
+	public void postAction(String action)
+	{
+		if(action.contains("stack"))
+			{
+				//add
+				armempty = 1;
+				on[action.charAt(6)%97][action.charAt(8)%97] = 1;
+				clear[action.charAt(6)%97] = 1;
+				
+				//delete
+				clear[action.charAt(8)%97] = 0;
+				holding= 0;
+			}
+			else if(action.contains("unstack"))
+			{
+				//add
+				holding = action.charAt(8);
+				clear[action.charAt(10)%97] = 1;
+				
+				//delete
+				armempty = 0;
+				on[action.charAt(8)%97][action.charAt(10)%97] = 0;
+
+			}
+			else if(action.contains("pickup"))
+			{
+				//add
+				holding = action.charAt(7);
+				
+				//delete
+				armempty = 0;
+				ontable[action.charAt(7)%97] = 0;
+				
+			}
+			else if(action.contains("putdown"))
+			{
+				//add
+				armempty = 1;
+				ontable[action.charAt(8)%97] = 0;
+				
+				//delete
+				holding = 0;
+
+			}
+
+	}
+
 	public void plan()
 	{
 		goalstack.push(goal);
-		String sg[] = start.split("['^']");
+		// String sg[] = start.split("['^']");
 		String current;
+
 		while(!goalstack.isEmpty())
 		{
+			System.out.println();
+			System.out.println(goalstack.clone());
+
 			current = goalstack.pop();
-			
-			if(check.contains("^"))
+			//System.out.println(check(current));
+			if(current.contains("stack") || current.contains("unstack")  || current.contains("pickup") || current.contains("putdown") )
 			{
-				if(check(current))
+				postAction(current);
+				actions.add(current);
+			}
+			else if(!check(current))
+			{
+				
+				if(current.contains("^"))
 				{
-					goalstack.pop();
-				}
-				else{
 					goalstack.push(current);
 					String sg[] = current.split("['^']");
 					for(int i=0;i<sg.length;i++)
 					{
-						goalstack.push(sg[i])
+						goalstack.push(sg[i]);
 					}
 				}
+				else{
+					preAction(current);
+				}
 			}
+
+
+
+
 		}
 		
 		
+	}
+
+	public void dispAction()
+	{
+		System.out.println("Actions:");
+		for(int i=0;i<actions.size();i++)
+		{
+			System.out.println(actions.get(i));
+		}
 	}
 
 	public static void main(String args[])
@@ -159,6 +290,9 @@ Scanner sc = new Scanner(System.in);
 		Planner p = new Planner(k);
 		p.input();
 		p.set_initial();
-	
+		p.plan();
+		p.dispAction();
+
+		s.close();
 	}
 }
